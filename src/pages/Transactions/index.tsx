e33547210aeff54e4c63723e76a2934ee7ebbc4e
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2023-01-13 11:10:00 -0300

    feat: created transaction table in transaction page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,35 @@
 import { Header, Summary } from "components";
 
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
+
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de site</td>
+              <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              <td>Venda</td>
+              <td>13/01/2023</td>
+            </tr>
+            <tr>
+              <td>Hamburguer</td>
+              <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
+              <td>Alimentação</td>
+              <td>13/01/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
---
+++ src/pages/Transactions/styles.ts
@@ -0,0 +1,55 @@
+import styled, { css, DefaultTheme } from "styled-components";
+
+export const TransactionsContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`;
+
+export const TransactionsTable = styled.table`
+  ${({ theme }) => css`
+    width: 100%;
+    border-collapse: separate;
+    border-spacing: 0 0.5rem;
+
+    color: ${theme["gray-300"]};
+
+    td {
+      padding: 1.25rem 2rem;
+
+      background-color: ${theme["gray-700"]};
+
+      &:first-child {
+        width: 50%;
+
+        border-top-left-radius: 6px;
+        border-bottom-left-radius: 6px;
+      }
+
+      &:last-child {
+        border-top-right-radius: 6px;
+        border-bottom-right-radius: 6px;
+      }
+    }
+  `}
+`;
+
+const priceHighLightModifiers = {
+  income: (theme: DefaultTheme) => css`
+    color: ${theme["green-300"]};
+  `,
+  outcome: (theme: DefaultTheme) => css`
+    color: ${theme["red-300"]};
+  `,
+};
+
+interface PriceHighLightProps {
+  variant: keyof typeof priceHighLightModifiers;
+}
+
+export const PriceHighLight = styled.td<PriceHighLightProps>`
+  ${({ theme, variant }) => css`
+    ${variant && priceHighLightModifiers[variant](theme)}
+  `}
+`;
