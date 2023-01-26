47672731f9ab35b55215e562b64af96473568f25
Guilherme Spica Luiz <Guilhermespicaluiz@gmail.com>
2023-01-25 23:46:34 -0300

    feat: created reduce values to summary cards

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ function App() {
       <TransactionProvider>
         <Transactions />
       </TransactionProvider>
-
       <GlobalStyle />
     </ThemeProvider>
   );
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,8 +1,31 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 
+import { useTransactions } from "contexts/TransactionsContext";
+
 import { SummaryCard, SummaryContainer } from "./styles";
 
 export function Summary() {
+  const { transactions } = useTransactions();
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  );
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -10,7 +33,7 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -18,7 +41,7 @@ export function Summary() {
           <span>Saídas</span>
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="total">
@@ -26,7 +49,7 @@ export function Summary() {
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
